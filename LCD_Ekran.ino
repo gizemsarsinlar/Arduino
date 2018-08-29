8c89167b272c68884d57e9e8d7a8509b984c46a6
Gizem <33731743+gizemsarsinlar@users.noreply.github.com>
2018-08-29 23:11:03 +0300

    Add files via upload

---
+++ LCD_Ekran.ino
@@ -0,0 +1,21 @@
+#include <LiquidCrystal.h> 
+LiquidCrystal LCD(11,10,9,2,3,4,5); 
+  
+void setup()
+{
+LCD.begin(16,2); 
+}
+  
+void loop() {
+int i; 
+LCD.setCursor(3,0);
+LCD.print("Gizem"); 
+for(i=0; i<17; i++)
+{
+LCD.setCursor(i,1);
+LCD.print("Sarsınlar");
+delay(1000);
+LCD.setCursor(i,1);
+LCD.print(" ");
+}
+}
